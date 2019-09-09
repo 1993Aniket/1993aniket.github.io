e6a5c90a6ea58c28d7ba2d08083e4a31c8037c3b
aniket <1993anikeet.jadhav@gmail.com>
2019-09-09 18:59:58 +0530

    efrewwerwe
    werwe

---
+++ old_html_site_latest/js/script.js
@@ -0,0 +1,166 @@
+/* 
+	author: istockphp.com
+*/
+jQuery(function($) {
+	
+	$("a.topopup").click(function() {
+			loading(); // loading
+			setTimeout(function(){ // then show popup, deley in .5 second
+				loadPopup(); // function show popup 
+			}, 500); // .5 second
+	return false;
+	});
+	
+	/* event for close the popup */
+	$("div.close").hover(
+					function() {
+						$('span.ecs_tooltip').show();
+					},
+					function () {
+    					$('span.ecs_tooltip').hide();
+  					}
+				);
+	
+	$("div.close").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$(this).keyup(function(event) {
+		if (event.which == 27) { // 27 is 'Ecs' in the keyboard
+			disablePopup();  // function close pop up
+		}  	
+	});
+	
+	$("div#backgroundPopup").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$('a.livebox').click(function() {
+		alert('Hello World!');
+	return false;
+	});
+	
+
+	 /************** start: functions. **************/
+	function loading() {
+		$("div.loader").show();  
+	}
+	function closeloading() {
+		$("div.loader").fadeOut('normal');  
+	}
+	
+	var popupStatus = 0; // set value
+	
+	function loadPopup() { 
+		if(popupStatus == 0) { // if value is 0, show popup
+			closeloading(); // fadeout loading
+			$("#toPopup").fadeIn(0500); // fadein popup div
+			$("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8
+			$("#backgroundPopup").fadeIn(0001); 
+			popupStatus = 1; // and set value to 1
+		}	
+	}
+		
+	function disablePopup() {
+		if(popupStatus == 1) { // if value is 1, close popup
+			$("#toPopup").fadeOut("normal");  
+			$("#backgroundPopup").fadeOut("normal");  
+			popupStatus = 0;  // and set value to 0
+		}
+	}
+	/************** end: functions. **************/
+}); // jQuery End
+
+
+
+
+/* 
+	author: istockphp.com
+*/
+jQuery(function($) {
+	
+	$("a.topopup1").click(function() {
+			loading(); // loading
+			setTimeout(function(){ // then show popup, deley in .5 second
+				loadPopup(); // function show popup 
+			}, 500); // .5 second
+	return false;
+	});
+	
+	/* event for close the popup */
+	$("div.close").hover(
+					function() {
+						$('span.ecs_tooltip').show();
+					},
+					function () {
+    					$('span.ecs_tooltip').hide();
+  					}
+				);
+	
+	$("div.close").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$(this).keyup(function(event) {
+		if (event.which == 27) { // 27 is 'Ecs' in the keyboard
+			disablePopup();  // function close pop up
+		}  	
+	});
+	
+	$("div#backgroundPopup").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$('a.livebox').click(function() {
+		alert('Hello World!');
+	return false;
+	});
+	
+
+	 /************** start: functions. **************/
+	function loading() {
+		$("div.loader").show();  
+	}
+	function closeloading() {
+		$("div.loader").fadeOut('normal');  
+	}
+	
+	var popupStatus = 0; // set value
+	
+	function loadPopup() { 
+		if(popupStatus == 0) { // if value is 0, show popup
+			closeloading(); // fadeout loading
+			$("#toPopup1").fadeIn(0500); // fadein popup div
+			$("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8
+			$("#backgroundPopup").fadeIn(0001); 
+			popupStatus = 1; // and set value to 1
+		}	
+	}
+		
+	function disablePopup() {
+		if(popupStatus == 1) { // if value is 1, close popup
+			$("#toPopup1").fadeOut("normal");  
+			$("#backgroundPopup").fadeOut("normal");  
+			popupStatus = 0;  // and set value to 0
+		}
+	}
+	/************** end: functions. **************/
+}); // jQuery End
+
+ 
+function MM_validateForm() { //v4.0
+  if (document.getElementById){
+    var i,p,q,nm,test,num,min,max,errors='',args=MM_validateForm.arguments;
+    for (i=0; i<(args.length-2); i+=3) { test=args[i+2]; val=document.getElementById(args[i]);
+      if (val) { nm=val.name; if ((val=val.value)!="") {
+        if (test.indexOf('isEmail')!=-1) { p=val.indexOf('@');
+          if (p<1 || p==(val.length-1)) errors+='- '+nm+' must contain an e-mail address.\n';
+        } else if (test!='R') { num = parseFloat(val);
+          if (isNaN(val)) errors+='- '+nm+' must contain a number.\n';
+          if (test.indexOf('inRange') != -1) { p=test.indexOf(':');
+            min=test.substring(8,p); max=test.substring(p+1);
+            if (num<min || max<num) errors+='- '+nm+' must contain a number between '+min+' and '+max+'.\n';
+      } } } else if (test.charAt(0) == 'R') errors += '- '+nm+' is required.\n'; }
+    } if (errors) alert('The following error(s) occurred:\n'+errors);
+    document.MM_returnValue = (errors == '');
+} }
